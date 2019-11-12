111af07ec8c54d670839d6908c386fa50481a831
Amanda Rolon <abrolon87@gmail.com>
2019-11-12 17:06:03 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,36 +1,52 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
+
 # PORTION_1 = {
 #   label: "Kingdom",
 #   sub_category: {
-#     label: "Phylum",
+#   label: "Phylum",
 #     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
+#     label: "Class"
+#    }
+#  }
+#}
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
 
 def naming_system
+  @Kingdom = {
+    :sub_category=> {
+      :label=>"Phylum",
+      :sub_category=> {
+        :label=> "Class",
+        :sub_category=> {
+          :label=> "Order",
+          :sub_category=> {
+            :label=> "Family",
+            :sub_category=> {
+              :label=> "Genus",
+              :sub_category=> {
+                :label=> "Species",
+              }
+            }
+          }
+        }
+        }
+      }
+  }
+
   # Remember:
   #  Kingdom
   #  Phylum
