b9bacec3d332458acebb68cf60640adadd6296c5
Alehandro Persaud <andrew041994@gmail.com>
2020-01-06 05:03:11 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,16 +46,15 @@ def naming_system
     :sub_category=>{
       :label=>"Phylum",
         :sub_category=>{
-          :label=>"Class"
-        }
-    },
-    :sub_category=>{
-      :label=>"Order"
-    },
-    :label=>"Family",
-    :sub_category=>{
-      :label=>"Genus"
-    },
-    :label=>"Species",
-    :sub_category=>nil
-  }
+          :label=>"Class",
+            :sub_category=>{
+              :label=>"Order",
+                :sub_category=>{
+                :label=>"Family",
+                  :sub_category=>{
+                    :label=>"Genus",
+                      :sub_category=>{
+                      :label=>"Species",
+                        :sub_category=>nil
+                      }}}}}}}
+    end
